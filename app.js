fc8f29db7852b6a5984e1d062f14c5251c8b5450
DavidZarfati <zarda2001@gmail.com>
2025-12-18 17:06:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express"
+import postRouter from "./routers/posts.js"
+
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+
+
+
+
+app.get("/", (req, resp) => {
+    console.log("Rotta /");
+    resp.send("test")
+});
+
+
+app.use("/posts", postRouter)
+
+// app.get("/bacheca", (req, res) => {
+//     console.log("sono console log dei post");
+//     const contatore = posts.length
+//     const postList = posts.map((post) => ({
+//         title: post.title,
+//         content: post.content,
+//         imageUrl: post.path,
+//         tags: post.tags,
+//         id: post.id,
+//         totalePost: contatore
+//     }))
+//     res.json(postList)
+// })
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("il server è in ascolto sulla porta " + port);
+})
