96488cb27507c5e5d4f9fdc2d69687af7455b7c1
DavidZarfati <zarda2001@gmail.com>
2026-01-08 16:08:16 +0100

    feat:aggiunto error Handrle e not found

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 import express from "express"
 import postRouter from "./routers/posts.js"
 import notFound from "./middlewares/notFound.js"
+import errorsHandler from "./middlewares/errorsHandler.js"
 
 const app = express()
 const port = 3000
-
 
 app.use(express.static("public"))
 
@@ -17,23 +18,13 @@ app.get("/", (req, resp) => {
     resp.send("test")
 });
 
-
+// Rotta di test per provare l'errorsHandler
+app.get("/test-error", (req, res) => {
+    throw new Error("Test errore");
+});
 app.use("/posts", postRouter)
 
-// app.get("/bacheca", (req, res) => {
-//     console.log("sono console log dei post");
-//     const contatore = posts.length
-//     const postList = posts.map((post) => ({
-//         title: post.title,
-//         content: post.content,
-//         imageUrl: post.path,
-//         tags: post.tags,
-//         id: post.id,
-//         totalePost: contatore
-//     }))
-//     res.json(postList)
-// })
-
+app.use(errorsHandler)
 app.use(notFound);
 
 app.listen(port, () => {
